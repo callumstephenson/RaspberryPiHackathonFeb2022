805622f216b7ba5377025a4e5fd330e39c2339b8
callumstephenson <callumstephenson03@gmail.com>
2022-02-26 19:44:02 +0000

    changed to up have delay = gesture number shown

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# RaspberryPiHackathonFeb2022
+cambridge engineering society x raspberry pi hackathon feb 2022.
+
+project includes image transmission from pi to remote client in order to do more intensive image processing. client.py to be run on remote machine and main.py to be run on the pi.
+
+pkg requirements:
+cv2, joblib, mediapipe, picamera2
+
+current functionality:
+-video feed transmitted over lan from pi to local machine
+-gesture recognition in order to take a photograph
+-currently signing a number will lead to a photograph being taken with a delay of "gesture number" seconds
+
+crsid involved:
+wy272, dg615, css47
--- gesture_recognition.py
+++ gesture_recognition.py
@@ -9,16 +9,16 @@ class gesture_response:
     # processes gesture and confidence level
     def gesture_action(self, gesture):
         self.gesture = gesture
-        if not self.busy_state and int(self.gesture) == 0:
+        if not self.busy_state:
             self.delay_capture()
 
     # function for delaying image capture
     def delay_capture(self):
         self.busy_state = True
-        print("Picture taking in 5!")
-        for i in range(1, 5):
+        print("Picture taking in ",int(self.gesture))
+        for i in range(1, self.gesture):
             time.sleep(1)
-            print(str(i))
+            print(str(int(self.gesture)-i))
         # capture_image(insert image capture function)       
         self.cam.capture_file("../images/{}.jpeg".format(int(time.time())))
         self.busy_state = False
